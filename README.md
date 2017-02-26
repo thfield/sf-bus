d65fdf7b616d5004022b3a3b579f13f6f245a4e7
thfield <thfield@gmail.com>
2017-02-26 14:29:29 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#sf-bus
+overlaying bus routes on a map.
+
+## data
+data from https://www.sfmta.com/about-sfmta/reports/gtfs-transit-data
+
+## libs
+[csv parse](http://csv.adaltas.com/parse/)
+[turf js](http://turfjs.org/)
+
+## steps
+1. download data from [sfmta](https://www.sfmta.com/about-sfmta/reports/gtfs-transit-data)
+1. unzip
+1. run makeGeoJson.js
+1. ???
+1. profit!
---
+++ makeGeoJsons.js
@@ -0,0 +1,82 @@
+"use strict";
+const start = new Date()
+
+const fs = require('fs')
+const csv = require('csv-parse')
+const turf = require('turf')
+
+const path = './google_transit/'
+
+let routes_file = 'routes.txt'
+let trips_file = 'trips.txt'
+let shapes_file = 'shapes.txt'
+
+/* Maps */
+let shapeRouteMap = new Map()
+let routeNameMap = new Map()
+
+/* read the csvs from fs */
+let routes = fs.readFileSync(path + routes_file, 'utf8')
+let trips = fs.readFileSync(path + trips_file, 'utf8')
+let shapes = fs.readFileSync(path + shapes_file, 'utf8')
+
+
+/* parse csv strings into objects */
+csv(routes, {columns: true}, parseRoutes)
+csv(trips, {columns: true}, parseTrips)
+csv(shapes, {columns: true}, parseShapes)
+
+function parseRoutes(err,data){
+  /* make name map */
+  if (err) console.error(err)
+  data.forEach(el=>{
+    routeNameMap.set(el.route_id,{shortName:el.route_short_name, longName:el.route_long_name})
+  })
+}
+
+function parseTrips(err, data){
+  /* make trip map */
+  if (err) console.error(err)
+  let tripz = {}
+
+  data.forEach(el=>{
+    tripz[el.shape_id] = tripz[el.shape_id] || []
+    tripz[el.shape_id].push(el.route_id)
+  })
+  for (let shapeID in tripz){
+    shapeRouteMap.set(tripz[shapeID][0],shapeID)
+  }
+}
+
+function parseShapes(err, data){
+  if (err) console.error(err)
+  let lineshapes = {}
+  data.forEach(el=>{
+    lineshapes[''+el.shape_id] = lineshapes[''+el.shape_id] || []
+    lineshapes[''+el.shape_id].push([el.shape_pt_lon,el.shape_pt_lat])
+  })
+  /* lineshapes is now k-v pairs, shape_id:[coordinates] */
+
+  // want to get by route
+  routeNameMap.forEach((props, routeId)=>{
+    let shapeId = shapeRouteMap.get(routeId)
+    let geoJSON = turf.lineString(lineshapes[shapeId], props)
+    write('shapefiles/' + geoJSON.properties.shortName + '.geo.json', geoJSON)
+  })
+
+  let elapsed = (new Date() - start) / 1000
+  console.log('took ' + elapsed + 's')
+}
+
+
+
+// output the file
+function write(filename, text){
+  if (typeof text != 'string') text = JSON.stringify(text)
+  fs.writeFile(filename, text,
+    function(err) {
+      if (err) { return console.log(err); }
+      console.log("The file was saved as", filename);
+    }
+  )
+}
