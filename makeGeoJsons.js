1bd435c3ffeb58d8fb16505cd798c6f67b03046e
thfield <thfield@gmail.com>
2017-03-04 15:48:53 -0800

    choose the most frequent trip as the representative lineshape

--- makeGeoJsons.js
+++ makeGeoJsons.js
@@ -44,45 +44,26 @@ function parseTrips(err, data){
 
   // TODO do something with the route / shape for weekend and late night trips
   data.forEach(el=>{
-    tripz[el.route_id] = tripz[el.route_id] || [ {},{},{},{} ]
-    if ( !tripz[el.route_id][el.service_id][el.shape_id] ) {
-      tripz[el.route_id][el.service_id][el.shape_id] = 1
-    } else {
-      tripz[el.route_id][el.service_id][el.shape_id] += 1
-    }
+    tripz[el.route_id] = tripz[el.route_id] || { }
+    tripz[el.route_id][el.service_id] = tripz[el.route_id][el.service_id] || {}
+    tripz[el.route_id][el.service_id][el.shape_id] =tripz[el.route_id][el.service_id][el.shape_id] || 0
+    tripz[el.route_id][el.service_id][el.shape_id] += 1
   })
-  // if ( shapeRouteMap.get(el.route_id + '-' + el.service_id) === undefined ) {
-  //   shapeRouteMap.set(el.route_id+ '-' + el.service_id, el.shape_id)
-  // }
-
-  let foo = {}
 
   for (let route in tripz) {
-
-    let weekday = mostCommonValue(tripz[route][1])
-    let saturday = mostCommonValue(tripz[route][2])
-    let sunday = mostCommonValue(tripz[route][3])
+    let weekday = mostCommonValue(tripz[route]['1'])
+    let saturday = mostCommonValue(tripz[route]['2'])
+    let sunday = mostCommonValue(tripz[route]['3'])
 
     shapeRouteMap.set(route+ '-1', weekday)
     shapeRouteMap.set(route+ '-2', saturday)
     shapeRouteMap.set(route+ '-3', sunday)
-    if ( shapeRouteMap.get(route + '-1') === undefined ) {
-      let mostCommon = mostCommonValue(route[1])
-      shapeRouteMap.set(route+ '-1', mostCommon)
-    }
-    if ( shapeRouteMap.get(route + '-2') === undefined ) {
-      let mostCommon = mostCommonValue(route[2])
-      shapeRouteMap.set(route+ '-2', mostCommon)
-    }
-    if ( shapeRouteMap.get(route + '-3') === undefined ) {
-      let mostCommon = mostCommonValue(route[3])
-      shapeRouteMap.set(route+ '-3', mostCommon)
-    }
   }
 }
 
 function mostCommonValue(obj) {
-   return Object.keys(obj).reduce(function(a, b){ return obj[a] > obj[b] ? a : b },0)
+  if(!obj){ return 0 }
+  return Object.keys(obj).reduce(function(a, b){ return obj[a] > obj[b] ? a : b }, 0)
 }
 
 function parseShapes(err, data){
@@ -94,39 +75,25 @@ function parseShapes(err, data){
   })
   /* lineshapes is now k-v pairs, shape_id:[coordinates] */
 
-  let foo = [
-    shapeRouteMap.get('1104-1')
-    // lineshapes['143406']
-  ]
-
-  write('temp.json', foo)
-
   // TODO write geojson linestrings for each shape, keyed by shape_id
   // want to get by route
-  // routeNameMap.forEach((props, routeId)=>{
-  //
-  //   let shapeId = shapeRouteMap.get(routeId + '-1')
-  //   // foo.push([props,Array.isArray(lineshapes[shapeId]) ])
-  //   if (shapeId === 0) {
-  //     shapeId = shapeRouteMap.get(routeId + '-2')
-  //   }
-  //   if (shapeId === 0) {
-  //     shapeId = shapeRouteMap.get(routeId + '-3 ')
-  //   }
-  //   let properties = {}
-  //   properties.routeId = routeId
-  //   properties.shapeId = shapeId
-  //   if( routeId == 1104 ){
-  //     console.log(routeId,shapeId,lineshapes[shapeId])
-  //   }
-  //   console.log(routeId)
-  //   let geoJSON = turf.lineString(lineshapes[shapeId], properties)
-  //   allRoutes.push(geoJSON)
-  //   write('shapefiles/' + geoJSON.properties.shortName + '.geo.json', geoJSON)
-  // })
-  //
-  // let fc = turf.featureCollection(allRoutes)
-  // write('shapefiles/all.geo.json', fc)
+  let foo = []
+  routeNameMap.forEach((props, routeId)=>{
+    let shapeId = shapeRouteMap.get(routeId + '-1')
+    if (shapeId === 0) {
+      shapeId = shapeRouteMap.get(routeId + '-2')
+    }
+    if (shapeId === 0) {
+      shapeId = shapeRouteMap.get(routeId + '-3')
+    }
+
+    let geoJSON = turf.lineString(lineshapes[shapeId], props)
+    allRoutes.push(geoJSON)
+    write('shapefiles/' + geoJSON.properties.shortName + '.geo.json', geoJSON)
+  })
+
+  let fc = turf.featureCollection(allRoutes)
+  write('shapefiles/all.geo.json', fc)
 
   let elapsed = (new Date() - start) / 1000
   console.log('took ' + elapsed + 's')
